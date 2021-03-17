a5cf19ef6aff78d0da6a51884d6aea3b2586a313
Ladislav Malecek <malecek.ladislav@gmail.com>
2021-03-17 22:35:58 +0100

    Adding db movie table, setting up python db access

---
+++ Recommender/Data/schema.py
@@ -0,0 +1,21 @@
+from peewee import Model, SqliteDatabase, TextField, DateField, IntegerField
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('../conf.ini')
+
+# db = SqliteDatabase(config['Database']['Filepath'])
+db = SqliteDatabase('../../app.db')
+
+class BaseTable(Model):
+    class Meta:
+        database = db
+
+class Movies(BaseTable):
+    id = IntegerField(null=False)
+    name = TextField(null=False, index=True)
+
+    class Meta:
+        database = db
+        table_name = "Movies"
---
+++ WebApp/Server/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroupMovieRecommender.Server.Models
+{
+    public class Movie
+    {
+        public static void Configure(EntityTypeBuilder<Movie> builder)
+        { }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
